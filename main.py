7a1c96aac2a1047cbed37d0492885f38f26fa18c
Raja Raman <raman.raja@gmail.com>
2019-03-10 07:00:23 +0000

    Pushed audio

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import urllib.request
 import time
+import RPi.GPIO as GPIO
+import pygame
+
 
 def isWebsiteUp(url):
     try:
         page = urllib.request.urlopen(url)
         # print (page.read())
         if page.getcode() == 200:
-            return True
+            GPIO.output(4,GPIO.HIGH)
+            pygame.mixer.music.load("/home/pi/lightson/raspberrypiprojects/audio/up.mp3")
+            pygame.mixer.music.play()
         else:
-            return False
+            pygame.mixer.music.load("/home/pi/lightson/raspberrypiprojects/audio/down.mp3")
+            pygame.mixer.music.play()
+            GPIO.output(4,GPIO.LOW)
     except:
-        return False
+        pygame.mixer.music.load("/home/pi/lightson/raspberrypiprojects/audio/down.mp3")
+        pygame.mixer.music.play()
+        GPIO.output(4,GPIO.LOW)
 
 def periodicCheckWebsiteUp(url, sleeptime):
     while True:
         print(isWebsiteUp(url))
         time.sleep(sleeptime)
-
+        
+def piSetup():
+    
+    #Setup GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(4,GPIO.OUT)
+    
+    # Setup audio
+    pygame.mixer.init()
+    
+piSetup()
 periodicCheckWebsiteUp("http://ec2-13-127-120-28.ap-south-1.compute.amazonaws.com", 5)
